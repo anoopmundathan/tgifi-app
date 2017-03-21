ef7780d0776c48159bce1acaf3a5f78c4ae9b6e8
anoopmundathan <anoop.mundathan@gmail.com>
2017-03-21 17:36:31 +0000

    Setup views in pug

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 'use express';
 
 var express = require('express');
+var path = require('path');
 
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res, next) {
+	res.render('login');
+});
+
+app.post('/login', function(req, res, next) {
+	res.send('submitted');
+});
 
 app.use(function(req, res, next) {
 	var err = new Error('Resource not found');
