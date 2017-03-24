205b7f5039256c715a29d1996c3b79d40795d4f3
anoopmundathan <anoop.mundathan@gmail.com>
2017-03-24 05:51:44 +0000

    Add mocha for unit testing

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+// Test suite
+describe('Mocha', function() {
+	// Test spec
+	it('Should use npm', function() {
+		expect(true).to.be.false;
+	});
+});
