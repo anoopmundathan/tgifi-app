900e5d674d3630d3d7d70592971b1abd426fb1ab
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-07 04:57:34 +0100

    Store user to session

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,35 +12,27 @@ router.get('/', function(req, res) {
 	res.send('auth page');
 });
 
+
+function saveToSession(req, res, next) {
+	req.session.token = jwt.sign(req.user._id, secret);
+	req.session.user  = req.user.userName
+	return res.redirect('/');
+}
+
+
 // GET /auth/login/facebook
 router.get('/login/facebook', passport.authenticate('facebook', { scope: ["email"]}));
-
-// GET /auth/facebook/return
-router.get('/facebook/return', 
-	passport.authenticate('facebook', { failureRedirect: '/' }),
+router.get('/facebook/return', passport.authenticate('facebook', { failureRedirect: '/' }), 
 	function(req, res, next) {
-
-		// Save token
-		req.session.token = jwt.sign(req.user, secret);
-		req.session.user  = req.user
-
-		return res.redirect('/');
-});
-
+		saveToSession(req, res, next);
+	});
+	
 
 // GET /auth/login/github
 router.get('/login/github', passport.authenticate('github'));
-
-
-// GET /auth/github/return
-router.get('/github/return', passport.authenticate('github', {failureRedirect: '/'}),
-	function(req, res) {
-
-		// Save token
-		req.session.token = jwt.sign(req.user, secret);
-		req.session.user  = req.user
-
-		return res.redirect('/');
+router.get('/github/return', passport.authenticate('github', {failureRedirect: '/'}), 
+	function(req, res, next) {
+		saveToSession(req, res, next);
 	});
-
+	
 module.exports = router;
